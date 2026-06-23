f530433e684c5a1f1abd6ff7729a3a56294f5095
Panduadi78 <panduadikusuma13@gmail.com>
2026-06-24 05:18:13 +0700

    add google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -845,6 +845,18 @@
             updateIcon();
         </script>
 
+        
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+            gtag('config', 'G-QCGZX6GPQG');
+        </script>
+
     </body>
 
     </html>
